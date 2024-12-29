0ff17fecfba169c301c6353d311817dbd1bf59ee
Stefanus Adri Irawan <148773999+stefanus-ai-tech@users.noreply.github.com>
2024-12-30 00:45:26 +0700

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NWWGZW5Y82"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-NWWGZW5Y82');
+</script>
